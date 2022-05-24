0335d9adc286c37d2561f8c4d6f9fc77c79ec5f7
Oscar Miras <oscar.miras@gmail.com>
2022-05-24 12:13:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,29 +6,4 @@ const pictures = [
   "contBcg-4"
 ];
 
-Vue.createApp({
-  data() {
-    return {
-      index: 3
-    }
-  },
-  methods: {
-    prevImage() {
-      // TODO: Decrementar el index
-      this.index--;
-    },
-
-    nextImage() {
-      // TODO: Incrementar el index
-      this.index++;
-    }
-  },
-  computed: {
-    getUrl() {
-      const realIndex = Math.abs(this.index) % pictures.length;
-      return `url(../img/${pictures[realIndex]}.jpeg)`;
-      // tenemos que devolver un string a la ruta de la foto actual. Por ejemplo, la ruta de la primera foto es '../img/pictures[0]'
-    }
-  }
-
-}).mount('#app')
+// buena suerte
