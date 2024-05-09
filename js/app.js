367ac291a24314aa09b71a4dca8562f6ab817aad
zulcas <zulemacasadogarcia@gmail.com>
2024-05-09 19:18:48 +0200

    ejercicio resuelto

--- js/app.js
+++ js/app.js
@@ -10,3 +10,49 @@ const pictures = [
  * ¿Qué variable de estado vamos a necesitar?
  * ¿Cómo se establece la imagen de fondo de un elemento mediante un estilo en línea?
  */
+
+//Creamos las funciones slider left y right
+function sliderMoveLeft (){
+  
+  if(pictureNumber > 0){
+    pictureNumber --;
+    imageContainer.style.backgroundImage = `url('${pictures[pictureNumber]}')`;
+    console.log(pictureNumber)
+  }
+  else{
+    pictureNumber = pictures.length-1;
+    imageContainer.style.backgroundImage = `url('${pictures[pictureNumber]}')`;
+    console.log(pictureNumber);
+
+  }
+
+}
+function sliderMoveRight(){
+  if(pictureNumber < pictures.length-1){
+    pictureNumber++;
+    imageContainer.style.backgroundImage = `url('${pictures[pictureNumber]}')`;
+    console.log(pictureNumber)
+  }
+  else{
+    pictureNumber = 0;
+    imageContainer.style.backgroundImage = `url('${pictures[pictureNumber]}')`;
+    console.log(pictureNumber)
+  }
+
+}
+
+//Variables de estado
+let pictureNumber = 0;
+let imageContainer = document.querySelector('.img-container');
+
+//Inicializamos primera imagen
+document.querySelector('.img-container').style.backgroundImage = `url('${pictures[0]}')`;
+
+
+//1)Creamos evento click para cada boton
+let buttonLeft = document.querySelector('.btn-left');
+let buttonRight = document.querySelector('.btn-right');
+
+//2)Asociamos click a cada boton
+buttonLeft.addEventListener('click', sliderMoveLeft)
+buttonRight.addEventListener('click', sliderMoveRight)
